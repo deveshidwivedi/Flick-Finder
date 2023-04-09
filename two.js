0474fe8fb74f9dec9cfe853bee7f52102f899bf5
deveshidwivedi <deveshigurgaon@gmail.com>
2023-04-09 17:28:28 +0530

    initial commit

---
+++ two.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    document.querySelector(".popup").style.display = "block";
+    var popup = window.open("link.html", "popup", "width=300,height=200");
+}
+document.getElementsByClassName('close-btn')[0].onclick = () => {
+    //alert("test");
+    //write code...
+}
